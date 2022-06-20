7297578f0d25242de848c496715c283990f6fde3
Holger Hinzberg <holger@hinzberg.de>
2022-06-20 20:45:43 +0200

    Initial Commit

---
+++ NavigationStack_Demo_SwiftUI/NavigationStack_Demo_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStack_Demo_SwiftUIApp.swift
+//  NavigationStack_Demo_SwiftUI
+//
+//  Created by Holger Hinzberg on 20.06.22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStack_Demo_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
